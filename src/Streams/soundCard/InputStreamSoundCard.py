3f41d15b918892177c45bd127db288c2b1859bf6
xopheS <xophesaad@gmail.com>
2019-11-25 18:31:13 +0100

    Updated requirements

--- src/Streams/soundCard/InputStreamSoundCard.py
+++ src/Streams/soundCard/InputStreamSoundCard.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Nov 18 14:08:04 2019
-
-@author: nizar
-"""
 import sys
 import sounddevice as sd #low level library for soundcard(hardware) use
 sys.path.append('../../')
@@ -12,8 +5,19 @@ import Preconditions as p
 from Streams.Stream import Stream
 from Streams.Tracks import Track 
 
+## InputStream_SoundCard
+#
+# Input stream for sound card
 class InputStream_SoundCard (Stream):
     
+    ## InputStream_SoundCard constructor
+    #  @param self Object's pointer
+    #  @param nchannels Number of channels
+    #  @param framerate The framerate
+    #  @param dtype Format of data
+    #  @param device Recording device
+    #  @param infinite Boolean indicating if stream infinite
+    #  @param launch Boolean indicating if input stream opened at initialization
     def __init__ (self, nchannels, framerate, dtype=sd.default.dtype, device= sd.default.device, infinite= True, launch = True):
         self.framerate = framerate
         self.nchannels = nchannels
@@ -22,14 +26,22 @@ class InputStream_SoundCard (Stream):
         super().__init__(device, infinite, launch)
         
         
+    ## Open method of sound card input stream
+    #  @param self Object's pointer
     def open (self):
         self.launched = True
         self.stream.start()
         self.samplewidth = self.stream.samplesize
         
+    ## Close method of sound card input stream
+    #  @param self Object's pointer
     def close (self): 
         self.stream.stop()
         
+    ## Read from soundcard
+    #  @param self Object's pointer
+    #  @param n number of frames
+    #  @return a track containing the record
     def read(self, n):
         p.check(n <= self.time() and self.launched)
         return Track(self.stream.read(n)[0], n, self.nchannels, self.samplewidth, self.framerate)
