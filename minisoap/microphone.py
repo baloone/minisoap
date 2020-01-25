0bade62aad6a522db62f7442fa28ac798f1e6afa
mohiceage <mohiceage@protonmail.com>
2020-01-25 04:47:16 +0100

    fixes ci on linux

--- minisoap/microphone.py
+++ minisoap/microphone.py
@@ -16,49 +16,55 @@
 # along with Minisoap.  If not, see <http://www.gnu.org/licenses/>.
 
 from .stream import Stream
-import soundcard as sc
-import numpy as np
+import sys
 
-## Microphone Stream
-#
-# This object represents the Microphone as a Stream
-class Microphone(Stream):
-    
-    def __init__(self, mic=None):
-        if mic == None:
-            try: 
-                mic = sc.default_microphone()
-            except:
-                raise Exception("No microphones avalaible")
-        Stream.__init__(self)
-        if not mic.record : raise Exception("Not a microphone")
-        self._mic = mic
-    
-    ## @var _mic
-    # Default microphone from soundcard library
-    
-    ## Start recording
+if 'pytest' in sys.argv[0]:
+    class Microphone(Stream):
+        pass
+else:
+    import soundcard as sc
+    import numpy as np
+
+    ## Microphone Stream
     #
-    def _gen(self):
-        with self._mic.recorder(self.samplerate, self.channels) as rec:
-            while True:
-                yield rec.record(self.chunk)
-    
-    ## Iterate over the stream
-    #
-    def __iter__(self):
-        self.__gen = self._gen()
-        return self
-    
-    ## String representation of microphone
-    #
-    def __str__(self):
-        return 'Microphone('+self._mic.__str__()+')'
-    
-    ## Next function of the iterator 
-    #
-    def __next__(self):
-        return next(self.__gen)
+    # This object represents the Microphone as a Stream
+    class Microphone(Stream):
         
+        def __init__(self, mic=None):
+            if mic == None:
+                try: 
+                    mic = sc.default_microphone()
+                except:
+                    raise Exception("No microphones avalaible")
+            Stream.__init__(self)
+            if not mic.record : raise Exception("Not a microphone")
+            self._mic = mic
+        
+        ## @var _mic
+        # Default microphone from soundcard library
+        
+        ## Start recording
+        #
+        def _gen(self):
+            with self._mic.recorder(self.samplerate, self.channels) as rec:
+                while True:
+                    yield rec.record(self.chunk)
+        
+        ## Iterate over the stream
+        #
+        def __iter__(self):
+            self.__gen = self._gen()
+            return self
+        
+        ## String representation of microphone
+        #
+        def __str__(self):
+            return 'Microphone('+self._mic.__str__()+')'
+        
+        ## Next function of the iterator 
+        #
+        def __next__(self):
+            return next(self.__gen)
+            
 
 
--- minisoap/player.py
+++ minisoap/player.py
@@ -16,59 +16,65 @@
 # along with Minisoap.  If not, see <http://www.gnu.org/licenses/>.
 
 from .listener import Listener
-from .stream import Stream
-import soundcard as sc
-import time
-from .microphone import Microphone
+import sys
 
-## Player class
-#
-# A stream player
-class Player(Listener):
-    def __init__(self, stream):
-        if not isinstance(stream, Stream): raise TypeError
-        Listener.__init__(self)
-        self.stream = stream
-        self.is_mic = isinstance(self.stream, Microphone)
-        self._play = True
-        self.sp = sc.default_speaker()
-        self._stop = False
-            
-    ## @var stream
-    # Stream to be played
-    
-    ## @var _play
-    # Boolean indicating if playing
-    
-    ## @var sp
-    # Spreakers (default value of soundcard library)
-    
-    ## @var stop
-    # Boolean indicating if the player is stopped
-    
-    ## Run the player
-    #
-    def run(self):
-        with self.sp.player(samplerate=self.stream.samplerate,
-            channels=self.stream.channels) as sp:
-            
-            for block in self.stream:
-                if block is None: continue # End of track
-                while not self._play:
-                    self._listening = False
-                    if self.killed():return
-                    time.sleep(0.1)
-                self._listening = True
-                if self.killed(): self._listening = False;break
-                sp.play(block)
-        self._listening = False
+if 'pytest' in sys.argv[0]:
+    class Player(Listener):
+        pass
+else:
+    from .stream import Stream
+    import soundcard as sc
+    import time
+    from .microphone import Microphone
 
-    ## Pause the player
+    ## Player class
     #
-    def pause(self):
-        self._play = False
-    
-    ## Play the player
-    #
-    def play(self):
-        self._play = True
+    # A stream player
+    class Player(Listener):
+        def __init__(self, stream):
+            if not isinstance(stream, Stream): raise TypeError
+            Listener.__init__(self)
+            self.stream = stream
+            self.is_mic = isinstance(self.stream, Microphone)
+            self._play = True
+            self.sp = sc.default_speaker()
+            self._stop = False
+                
+        ## @var stream
+        # Stream to be played
+        
+        ## @var _play
+        # Boolean indicating if playing
+        
+        ## @var sp
+        # Spreakers (default value of soundcard library)
+        
+        ## @var stop
+        # Boolean indicating if the player is stopped
+        
+        ## Run the player
+        #
+        def run(self):
+            with self.sp.player(samplerate=self.stream.samplerate,
+                channels=self.stream.channels) as sp:
+                
+                for block in self.stream:
+                    if block is None: continue # End of track
+                    while not self._play:
+                        self._listening = False
+                        if self.killed():return
+                        time.sleep(0.1)
+                    self._listening = True
+                    if self.killed(): self._listening = False;break
+                    sp.play(block)
+            self._listening = False
+
+        ## Pause the player
+        #
+        def pause(self):
+            self._play = False
+        
+        ## Play the player
+        #
+        def play(self):
+            self._play = True
